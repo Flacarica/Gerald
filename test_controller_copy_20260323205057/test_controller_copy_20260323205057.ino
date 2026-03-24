eb65f8c8ac96650e3bdc912523009f3b1af445f6
Tudor Flacarica <cosmin.popovici@students.2space.ro>
2026-03-24 02:22:21 +0200

    apgreid

--- test_controller_copy_20260323205057/test_controller_copy_20260323205057.ino
+++ test_controller_copy_20260323205057/test_controller_copy_20260323205057.ino
@@ -9,9 +9,16 @@
 #define MA2 14
 #define MB1 25
 #define MB2 26
+#define MC1 33
+#define MC2 32
+#define MD1 34
+#define MD2 35
+
 
 #define PWMA 4
 #define PWMB 17
+#define PWMC 18
+#define PWMD 5
 
 void printDeviceAddress() {
   const uint8_t* point = esp_bt_dev_get_address();
@@ -36,8 +43,14 @@ void setup() {
   pinMode(MA2, OUTPUT);
   pinMode(MB1, OUTPUT);
   pinMode(MB2, OUTPUT);
+  pinMode(MC1, OUTPUT);
+  pinMode(MC2, OUTPUT);
+  pinMode(MD1, OUTPUT);
+  pinMode(MD2, OUTPUT);
   pinMode(PWMA, OUTPUT);
   pinMode(PWMB, OUTPUT);
+  pinMode(PWMC, OUTPUT);
+  pinMode(PWMD, OUTPUT);
 
   Serial.println("Outputs enabled ...");
 
@@ -45,8 +58,14 @@ void setup() {
   digitalWrite(MA2, LOW);
   digitalWrite(MB1, LOW);
   digitalWrite(MB2, LOW);
+  digitalWrite(MC1, LOW);
+  digitalWrite(MC2, LOW);
+  digitalWrite(MD1, LOW);
+  digitalWrite(MD2, LOW);
   analogWrite(PWMA, 0);
   analogWrite(PWMB, 0);
+  analogWrite(PWMC, 0);
+  analogWrite(PWMD, 0);
 
   Serial.println("Outputs set to low ...");
 
@@ -72,18 +91,30 @@ void loop() {
         digitalWrite(MA2, LOW);
         digitalWrite(MB1, LOW);
         digitalWrite(MB2, LOW);
+        digitalWrite(MC1, LOW);
+        digitalWrite(MC2, LOW);
+        digitalWrite(MD1, LOW);
+        digitalWrite(MD2, LOW);
         analogWrite(PWMA, 0);
         analogWrite(PWMB, 0);
+        analogWrite(PWMC, 0);
+        analogWrite(PWMD, 0);
       } else {
         while(PS4.R2Value()>0){
           //  Setam motoarele pe directia inainte
           digitalWrite(MA1, HIGH);
           digitalWrite(MA2, LOW);
-          digitalWrite(MB1, LOW);
-          digitalWrite(MB2, HIGH);
+          digitalWrite(MB1, HIGH);
+          digitalWrite(MB2, LOW);
+          digitalWrite(MC1, HIGH);
+          digitalWrite(MC2, LOW);
+          digitalWrite(MD1, HIGH);
+          digitalWrite(MD2, LOW);
           //  Acceleram cu cat citim de la butonul R2 de pe controller
           analogWrite(PWMA, PS4.R2Value());
           analogWrite(PWMB, PS4.R2Value());
+          analogWrite(PWMC, PS4.R2Value());
+          analogWrite(PWMD, PS4.R2Value());
 
           Serial.printf("R2 is at %d\n", PS4.R2Value());
 
@@ -92,15 +123,33 @@ void loop() {
           //  Setam motoarele pe directia inapoi
           digitalWrite(MA1, LOW);
           digitalWrite(MA2, HIGH);
-          digitalWrite(MB1, HIGH);
-          digitalWrite(MB2, LOW);
+          digitalWrite(MB1, LOW);
+          digitalWrite(MB2, HIGH);
+          digitalWrite(MC1, LOW);
+          digitalWrite(MC2, HIGH);
+          digitalWrite(MD1, LOW);
+          digitalWrite(MD2, HIGH);
           //  Acceleram cu cat citim de la butonul L2 de pe controller
           analogWrite(PWMA, PS4.L2Value());
           analogWrite(PWMB, PS4.L2Value());
+          analogWrite(PWMC, PS4.L2Value());
+          analogWrite(PWMD, PS4.L2Value());
 
           Serial.printf("L2 is at %d\n", PS4.L2Value());
 
         }
+        digitalWrite(MA1, LOW);
+        digitalWrite(MA2, LOW);
+        digitalWrite(MB1, LOW);
+        digitalWrite(MB2, LOW);
+        digitalWrite(MC1, LOW);
+        digitalWrite(MC2, LOW);
+        digitalWrite(MD1, LOW);
+        digitalWrite(MD2, LOW);
+        analogWrite(PWMA, 0);
+        analogWrite(PWMB, 0);
+        analogWrite(PWMC, 0);
+        analogWrite(PWMD, 0);
     }
   }
   //  Daca s-a deconectat controllerul, oprim motoarele
